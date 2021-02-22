3a431fc8e35273d17f2e5c40023207676de448b9
Josias D. Rodriguez <yosiprompt@gmail.com>
2021-02-22 14:45:06 -0400

    design fix

--- resources/views/message/create.blade.php
+++ resources/views/message/create.blade.php
@@ -8,7 +8,7 @@
         <form method="POST" action="{{ route('message.store') }}">
             @csrf
             <div>
-                <textarea name="message" id="message" cols="30" rows="5" placeholder="Write your message here"></textarea>
+                <textarea name="message" id="message" cols="30" rows="5" placeholder="Write your message here" class="rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></textarea>
             </div>
             <div class="flex items-center justify-end mt-4">
                 <x-button class="ml-4">
